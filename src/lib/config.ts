dac620a35b2714ff53a650e2e5bcc82d5cdbb61e
Ben Ford <ben.ford@servicem8.com>
2025-04-29 16:06:57 +0930

    Updated SDK

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -61,7 +61,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.3.9",
+  sdkVersion: "0.3.10",
   genVersion: "2.593.4",
-  userAgent: "speakeasy-sdk/typescript 0.3.9 2.593.4 1.0.0 servicem8",
+  userAgent: "speakeasy-sdk/typescript 0.3.10 2.593.4 1.0.0 servicem8",
 } as const;
